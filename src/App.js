4eee5242ff0763981011f18e8b6cff779794594d
xiaofan <1353815633@qq.com>
2018-06-18 17:50:04 +0800

    add server

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React, { Component } from 'react';
 
-class ContactsList extends Component {
-  render() {
-    const people = this.props.contacts;
-
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <ContactsList contacts={[
-          {name: 'xiaoming'},
-          {name: 'xiaofan'},
-          {name: 'hehehaha'}
-        ]}/>
-        <ContactsList contacts={[
-          {name: 'meixi'},
-          {name: 'cluo'}
-        ]}/>
-      </div>
-    );
+    return <div>hello xiaoming</div>
   }
 }
 
