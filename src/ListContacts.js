1f4c661f966a2ba000a21976264ee5b508767f99
xiaofan <1353815633@qq.com>
2018-06-18 18:59:25 +0800

    显示联系人 头像、名字、和email

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -5,8 +5,16 @@ class ListContacts extends Component {
     return (
       <ol className='contact-list'>
 				{this.props.contacts.map((contact) => (
-					<li key={contact.id}>
-						{contact.name}
+					// Each child in an array or iterator should have a unique "key" prop.
+					<li key={contact.id} className='contact-list-item'>
+						<div className='contact-avatar' style={{
+							backgroundImage: `url(${contact.avatarURL})`
+						}}/>
+						<div className='contact-details'>
+							<p>{contact.name}</p>
+							<p>{contact.email}</p>
+						</div>
+						<button className='contact-remove'></button>
 					</li>
 				))}
       </ol>
