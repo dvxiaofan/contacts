d5f7769f7c0a27089bfdb46cace675080f50c4b7
xiaofan <1353815633@qq.com>
2018-06-19 15:45:55 +0800

    增加搜索框， 并实时获取输入状态

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,29 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-function ListContacts(props) {
-	return (
-		<ol className='contact-list'>
-				{props.contacts.map((contact) => (
-					// Each child in an array or iterator should have a unique "key" prop.
-					<li key={contact.id} className='contact-list-item'>
-						<div className='contact-avatar' style={{
-							backgroundImage: `url(${contact.avatarURL})`
-						}}/>
-						<div className='contact-details'>
-							<p>{contact.name}</p>
-							<p>{contact.email}</p>
-						</div>
-						<button onClick={() => props.onDeleteContact(contact)} className='contact-remove'></button>
-					</li>
-				))}
-      </ol>
-	)
-}
-// 利用proptypes 指定props 的类型和是否为必须
-ListContacts.propTypes = {
-	contacts: PropTypes.array.isRequired,
-	onDeleteContact: PropTypes.func.isRequired
+class ListContacts extends Component {
+	// 利用proptypes 指定props 的类型和是否为必须
+	static propTypes = {
+		contacts: PropTypes.array.isRequired,
+		onDeleteContact: PropTypes.func.isRequired
+	}
+
+	state = {
+		query: ''
+	}
+
+	updatedQuery = (query) => {
+		// 调用 setState()，合并新状态以更新组件的内部状态
+		this.setState({ query: query.trim() })
+	}
+
+	render() {
+		return (
+			<div className='list-contacts'>
+				{JSON.stringify(this.state)}
+				<div className='list-contacts-top'>
+					<input 
+						className='search-contacts' 
+						type='text' 
+						placeholder='Search contacts'
+						value={this.state.query}
+						// 事件监听器在每个 onChange 事件上触发 updateQuery() 函数
+						onChange={(event) => this.updatedQuery(event.target.value)}
+						/>
+				</div>
+				<ol className='contact-list'>
+					{this.props.contacts.map((contact) => (
+						// Each child in an array or iterator should have a unique "key" prop.
+						<li key={contact.id} className='contact-list-item'>
+							<div className='contact-avatar' style={{
+								backgroundImage: `url(${contact.avatarURL})`
+							}}/>
+							<div className='contact-details'>
+								<p>{contact.name}</p>
+								<p>{contact.email}</p>
+							</div>
+							<button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'></button>
+						</li>
+					))}
+				</ol>
+			</div>
+		)
+	}
 }
 
 export default ListContacts
